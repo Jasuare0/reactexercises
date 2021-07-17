e461b30d4527f2ab1efa56c5b0e7d0198f1b7977
Javier Suarez <javiersuarezdominguez@gmail.com>
2021-07-17 20:32:29 -0300

    Component Navbar created importing from a module

---
+++ Clase-02/mi-primer-proyecto/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+let Navbar = () => {
+    return(
+        <nav>
+            <ul>
+                <li>Lista 1</li>
+                <li>Lista 2</li>
+                <li>Lista 3</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
